d4b64a45172c0c18a03f583e2a0bfd10425f105a
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-01-23 13:09:13 +0200

    task 1 at work

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyRef = document.querySelector('body');
+const btnStartRef = document.querySelector('[data-start]');
+const btnStoptRef = document.querySelector('[data-stop]');
 
+const DELAY = 1000;
+let timerId = null;
+
+btnStartRef.addEventListener('click', onBtnStartClick);
+btnStoptRef.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  timerId = setInterval (changeBodyBackgroundColor, DELAY);
+}
+
+function onBtnStopClick () {
+  clearInterval(timerId);
+}
+
+function changeBodyBackgroundColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  console.log('hi');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
